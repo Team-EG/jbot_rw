0d86820356beb0651dde98f99484e40eaf8212c1
eunwoo1104 <sions04@naver.com>
2020-08-17 17:55:56 +0900

    aaaaaaaaaa

--- cogs/help.py
+++ cogs/help.py
@@ -1,5 +1,6 @@
 import discord
 import json
+import koreanbots
 from discord.ext import commands
 from modules import page
 
@@ -24,6 +25,22 @@ prohibited_cogs = ["Dev", "Error", "Presence", "EasterEgg"]
 class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        with open("bot_settings.json", "r") as f:
+            bot_settings = json.load(f)
+            token = bot_settings["koreanbots_token"]
+        self.client = koreanbots.Client(self.bot, token, postCount=False)
+
+    async def cog_before_invoke(self, ctx):
+        try:
+            res = await self.client.getVote(ctx.author.id)
+            if res.response["voted"]:
+                return
+        except koreanbots.NotFound:
+            pass
+        embed = discord.Embed(title="잠시만요!",
+                              description="아직 코리안봇에서 제이봇에게 하트를 누르지 않으셨네요...\n지금 [여기를 눌러서](https://koreanbots.dev/bots/622710354836717580) 코리안봇에 투표해주세요!",
+                              color=discord.Color.red())
+        await ctx.send(embed=embed)
 
     @commands.group(name="도움", description="봇의 도움말 명령어를 출력합니다.", aliases=["도움말", "help"])
     async def help(self, ctx):
@@ -63,8 +80,16 @@ class Help(commands.Cog):
         await page.start_page(self.bot, ctx=ctx, lists=embed_list, embed=True)
 
     @help.command(name="검색")
-    async def help_search(self, ctx):
-        pass
+    async def help_search(self, ctx, cmd_name):
+        cogs = [(x, y.get_commands()) for x, y in self.bot.cogs.items()]
+        for x in cogs:
+            for n in x[1]:
+                if cmd_name == n.name:
+                    embed = discord.Embed(title=f"{cmd_name} 명렁어 정보", description=str(n.description), color=discord.Color.from_rgb(225, 225, 225))
+                    embed.add_field(name="사용법", value=str(n.usage) if n.usage else f"`{n.name}`", inline=False)
+                    embed.add_field(name="에일리어스", value=', '.join(n.aliases) if bool(n.aliases) else "없음", inline=False)
+                    return await ctx.send(embed=embed)
+        await ctx.send(f"`{cmd_name}`(은)는 없는 명령어입니다.")
 
 
 def setup(bot):
