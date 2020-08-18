d01f7febbd9691d2dbab045b97e076e2e2afda58
eunwoo1104 <sions04@naver.com>
2020-08-18 17:00:53 +0900

    added license to all codes

--- modules/lvl_calc.py
+++ modules/lvl_calc.py
@@ -1,3 +1,22 @@
+"""
+    jbot_rw
+    Copyright (C) 2020 Team EG
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 async def calc_req_exp(curr_lvl):
     desired_lvl = curr_lvl + 1
     lvl_up_req = 5 / 6 * desired_lvl * (2 * desired_lvl * desired_lvl + 27 * desired_lvl + 91)
