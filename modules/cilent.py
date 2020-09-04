a3ab61c5555d3cfbae458f5566f9e9eb971339f3
eunwoo1104 <sions04@naver.com>
2020-09-04 15:32:44 +0900

    취약점 수정

--- modules/cilent.py
+++ modules/cilent.py
@@ -14,12 +14,12 @@ class CustomClient(commands.AutoShardedBot):
         self.jbot_db_global = jbot_db.JBotDB("jbot_db_global")
         self.jbot_db_warns = jbot_db.JBotDB("jbot_db_warns")
         self.jbot_db_level = jbot_db.JBotDB("jbot_db_level")
-        self.stable_token = bot_settings["stable_token"]
-        self.canary_token = bot_settings["canary_token"]
+        self.__stable_token = bot_settings["stable_token"]
+        self.__canary_token = bot_settings["canary_token"]
         self.last_stock_change = 0
 
     def run_bot(self, canary=False):
         if canary is True:
-            self.run(self.canary_token)
+            self.run(self.__canary_token)
         else:
-            self.run(self.stable_token)
+            self.run(self.__stable_token)
