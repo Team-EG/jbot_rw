de3f0c52e506831e57b0453980b2ba2782aa2f81
eunwoo1104 <sions04@naver.com>
2020-12-21 20:24:33 +0900

    일단 마이너스는 막음

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -66,17 +66,17 @@ class Tasks(commands.Cog):
             for x in stock:
                 if len(stock) == 0:
                     break
-                random_price = random.randint(0, 60)
+                random_price = random.randint(0, 50)
                 random_percentage = random.randint(1, 100)
-                random_multipler = random.choice([1, 1, 1, 1, random.randint(2, 3)])
                 score = x["score"]
                 curr_price = x["curr_price"]
                 curr_history = x["history"].split(",")
                 if random_percentage < score: # 가격 하락
-                    curr_price -= random_price * random_multipler ** 2
+                    curr_price -= random_price
                     score -= random.randint(1, 10)
                     curr_history.append(str(curr_price))
                 else: # 가격 상승
+                    random_multipler = random.choice([1, 1, 1, 1, random.randint(2, 3)])
                     curr_price += random_price * random_multipler
                     score += random.randint(1, 10)
                     curr_history.append(str(curr_price))
